33a8ed2b77c45e18fa0e4afdab399e5f145d217c
Александр Марфицын <nonesasha225@gmail.com>
2021-01-02 08:20:14 +0500

    Добавьте файлы проекта.

---
+++ geekBrains_1.7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace geekBrains_1._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nums = 3;
+            bool flag = false;
+
+            for (int i = 0; i < nums; i++)
+            {
+                nums += i + (i * 2);
+            }
+            if (flag) Console.WriteLine(nums);
+            else Console.WriteLine("Тут ничего нет");
+            Console.ReadKey();
+        }
+    }
+}
